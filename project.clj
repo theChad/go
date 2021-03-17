c080dfca4823e3be9590d3a39222157b17bd5200
Chad <theChad@users.noreply.github.com>
2021-03-17 18:51:04 -0500

    min-lein-version to 2.0.0

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [quil "3.1.0"]]
+  :min-lein-version "2.0.0"
   :main ^:skip-aot go.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
