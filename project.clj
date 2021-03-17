c38decda2002a001432feb4ca184419cc6c1a6cb
Chad <theChad@users.noreply.github.com>
2021-03-17 18:40:45 -0500

    clojure version update

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [quil "3.1.0"]]
   :main ^:skip-aot go.core
   :target-path "target/%s"
